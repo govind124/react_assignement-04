9b85ee1ca507977438b8642867e9c1687de2930f
Pushpendra Singh <govindtop1@gmail.com>
2022-08-16 01:15:07 +0530

    react-assignment-4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Component/Navbar";
+import Home from "./Component/Home";
+import Student from "./Component/Student";
+import Contact from "./Component/Contact";
+// import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Student" element={<Student />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
---
+++ src/Component/Student.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './style.css'
+
+const Student = () => {
+  return (
+    <>
+       <h1>Devloper Record's</h1>
+        <table>
+            <tr>
+                <th>Collegues</th>
+                <th>Enroll No.</th>
+                <th>Fields </th>
+            </tr>
+            <tr>
+                <td>Pitambar Bhadra</td>
+                <td>01</td>
+                <td>Developer👨‍🔬</td>
+            </tr>
+            <tr>
+                <td>Rahul Yadav</td>
+                <td>02</td>
+                <td>Developer👨‍🔬</td>
+            </tr>
+            <tr>
+                <td>Atul Kumar Pandey</td>
+                <td>03</td>
+                <td>Developer👨‍🔬</td>
+            </tr>
+            <tr>
+                <td>Rupam Das</td>
+                <td>04</td>
+                <td>Developer👨‍🔬</td>
+            </tr>
+            <tr>
+                <td>Abinash Parida</td>
+                <td>05</td>
+                <td>Developer👨‍🔬</td>
+            </tr>
+             <tr>
+                <td>Nitin Kanhurkar</td>
+                <td>06</td>
+                <td>Developer👨‍🔬</td>
+            </tr>
+            <tr>
+                <td>Ritika Joshi</td>
+                <td>07</td>
+                <td>Developer👨‍🔬</td>
+            </tr>
+            <tr>
+                <td>Preeti</td>
+                <td>08</td>
+                <td>Developer👨‍🔬</td>
+            </tr>
+           
+        </table>
+    </>
+  )
+}
+
+export default Student
